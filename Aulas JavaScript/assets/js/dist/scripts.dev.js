598cda96533506eefce7852b92eab39d4e782645
Tassio <tassiosantana01@gmail.com>
2022-06-06 17:44:03 -0300

    introdução ao JavaScript

---
+++ Aulas JavaScript/assets/js/dist/scripts.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment() {
+  currentNumber = currentNumber + 1;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+  currentNumber = currentNumber - 1;
+  currentNumberWrapper.innerHTML = currentNumber;
+}
