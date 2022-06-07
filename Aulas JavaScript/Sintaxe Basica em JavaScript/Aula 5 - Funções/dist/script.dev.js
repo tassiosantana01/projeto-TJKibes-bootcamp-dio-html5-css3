44f3e3c741976fb6a9ee43f1143e0de734575baa
Tassio <tassiosantana01@gmail.com>
2022-06-07 15:53:02 -0300

    Aulas js

---
+++ Aulas JavaScript/Sintaxe Basica em JavaScript/Aula 5 - Funções/dist/script.dev.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function funcao() {
+  console.log('Testando funções!');
+}
+
+funcao();
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo);
+}
+
+mensagem('Testando', 'funções!');
---
+++ Aulas JavaScript/Sintaxe Basica em JavaScript/Aula 5 - Funções/script.js
@@ -0,0 +1,11 @@
+function funcao() {
+    console.log('Testando funções!');
+}
+
+funcao();
+
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo)
+}
+
+mensagem('Testando', 'funções!');
