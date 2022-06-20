b6c642bf3a2f02012d4225872cfc03844fee437e
Tassio <tassiosantana01@gmail.com>
2022-06-20 11:18:41 -0300

    jogo da memoria D&D

--- Jogo de Memoria D&D/dist/script.dev.js
+++ Jogo de Memoria D&D/dist/script.dev.js
@@ -1,1 +1,64 @@
 "use strict";
+
+var cards = document.querySelectorAll('.card');
+var hasFlippedCard = false;
+var firstCard, secondCard;
+var lockBoard = false;
+
+function flipcard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMath();
+}
+
+function checkForMath() {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCards();
+    return;
+  }
+
+  unflipCards();
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipcard);
+  secondCard.removeEventListener('click', flipcard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(function () {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+}
+
+(function shuffle() {
+  cards.forEach(function (card) {
+    var randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
+})();
+
+cards.forEach(function (card) {
+  card.addEventListener('click', flipcard);
+});
