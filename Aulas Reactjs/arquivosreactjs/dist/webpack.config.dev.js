7a66dad1249e9a928b3d2b53a9dc159b79bd13e3
Tassio <tassiosantana01@gmail.com>
2022-06-21 21:57:15 -0300

    introdução ao react

---
+++ Aulas Reactjs/arquivosreactjs/dist/webpack.config.dev.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var path = require('path');
+
+var HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundler.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
+      }
+    }]
+  },
+  plugins: [new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+  })]
+};
